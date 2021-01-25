8af7a70cc637c399f7ee7ea0e3634934e8c7a1e7
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-01-25 17:08:15 +0600

    redux set up with actions, reducers, store, container component, rootReducer

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,78 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            course: { title: "" }
+        };
+
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+    }
+
+    handleTitleChange(e) {
+        const course = this.state.course;
+        course.title = e.target.value;
+        this.setState({ course: course });
+    }
+
+    handleSave() {
+        // alert(`You are saving ${this.state.course.title}`);
+        // this.props.dispatch(courseActions.createCourse(this.state.course));
+        this.props.actions.createCourse(this.state.course)
+    }
+
+    courseRow(course, idx) {
+        return <div key={idx} > {course.title} </div>;
+    }
+
     render() {
         return (
             <div>
                 <h1>Courses Page</h1>
+                {this.props.courses.map(this.courseRow)}
+                <h2>Add courses</h2>
+                <input
+                    type="text"
+                    onChange={this.handleTitleChange}
+                    value={this.state.course.title}
+                />
+                <input
+                    type="submit"
+                    value="Save"
+                    onClick={this.handleSave}
+                />
             </div>
         );
     }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+    // dispatch: PropTypes.func.isRequired,
+    // createCourse: PropTypes.func.isRequired,
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        courses: state.courses
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        // createCourse : course => dispatch(courseActions.createCourse(course));
+        actions : bindActionCreators(courseActions, dispatch)
+    };
+};
+
+// const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
+// export default connectedStateAndProps(CoursesPage);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
